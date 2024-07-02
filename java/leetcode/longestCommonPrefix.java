a3560dd3da9632b794b38d2f7908d1ff12873a26
upgrace-in <thedesiretree47@gmail.com>
2024-07-02 16:43:37 +0530

    hari

--- java/leetcode/longestCommonPrefix.java
+++ java/leetcode/longestCommonPrefix.java
@@ -1,33 +1,28 @@
+import java.util.Arrays;
+
 class Solution {
 
-    public String longestCommonPrefix(String[] strs) {
-
-        String value = "";
-
-        for(int i=0; i<strs.length; i++) {
-
-            String partValue = "";
-            char currentChar = strs[i].charAt(i);
-            partValue = ""+currentChar;
-
-            for(int j=i+1; j<strs.length; j++){
-                if(currentChar != strs[j].charAt(i))
-                    return "";
-            }   
-
-            value = value + "" + partValue;
-
+    public String longestCommonPrefix(String[] v) {
+        StringBuilder ans = new StringBuilder();
+        Arrays.sort(v);
+        String first = v[0];
+        String last = v[v.length-1];
+        for (int i=0; i<Math.min(first.length(), last.length()); i++) {
+            if (first.charAt(i) != last.charAt(i)) {
+                return ans.toString();
+            }
+            ans.append(first.charAt(i));
         }
-
-        return value;
-
+        return ans.toString();
     }
 
     public static void main(String[] args){
         Solution s = new Solution();
         // String[] strs = {"flower","flow","flight"};
         // String[] strs = {""};
-        String[] strs = {"a"};
+        // String[] strs = {"a"};
+        // String[] strs = {"",""};
+        String[] strs = {"ab", "a"};
         System.out.println(s.longestCommonPrefix(strs));
     }
 }
